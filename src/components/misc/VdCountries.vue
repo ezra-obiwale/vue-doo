8590ec22acaf9148ebf399c5a3d5d1104625a8a2
Ezra Obiwale <contact@ezraobiwale.com>
2018-08-20 12:59:18 +0100

    Added countries component

---
+++ src/components/misc/VdCountries.vue
@@ -0,0 +1,134 @@
+<template>
+  <component
+    v-if="this.component"
+    :is="this.component"
+    v-bind="customBind"
+    v-on="$listeners"
+    v-model="model"
+    @reload="reload"
+    :error="error"
+    :countries="countries"
+    :loading="loading"
+    :message="message"
+    :options="countryOptions" />
+  <select
+    v-else
+    v-bind="$attrs"
+    v-on="$listeners"
+    :class="$attrs.class"
+    @click="reload"
+    @change="changed" >
+    <option value="" v-if="message">
+      {{ message }}
+    </option>
+    <template v-else
+      v-for="option in countryOptions">
+      <option v-if="option.value == value"
+        selected="selected"
+        :value="option.value"
+        :key="option.value">
+        {{ option.label }}
+      </option>
+      <option v-else
+        :value="option.value"
+        :key="option.value">
+        {{ option.label }}
+      </option>
+    </template>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'VdCountries',
+  props: {
+    bind: {
+      type: Function,
+      default: () => ({})
+    },
+    component: {
+      type: [Object, String]
+    },
+    loadErrorMessage: {
+      type: String,
+      default: 'Load failed!'
+    },
+    loadingMessage: {
+      type: String,
+      default: 'Loading ...'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      error: false,
+      loading: true,
+      countries: {}
+    }
+  },
+  computed: {
+    customBind () {
+      let attrs = this.$attrs,
+        customAttrs = this.bind(this)
+      return typeof customAttrs == 'object'
+        ? Object.assign({}, attrs, customAttrs)
+        : attrs
+    },
+    message () {
+      if (this.loading) {
+        return this.loadingMessage
+      } else if (this.error) {
+        return this.loadErrorMessage
+      }
+    },
+    model: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('change', value)
+      }
+    },
+    countryOptions () {
+      let options = []
+      this.countries.forEach(country => {
+        options.push({
+          label: country.name,
+          value: country.alpha2Code
+        })
+      })
+      return options
+    }
+  },
+  created () {
+    this.load()
+  },
+  methods: {
+    changed (e) {
+      this.$emit('change', e.target.value)
+    },
+    load () {
+      this.loading = true
+      $.get('//restcountries.eu/rest/v2/all')
+        .done(resp => {
+          this.loading = false
+          this.$set(this, 'countries', resp.data)
+        })
+        .error(resp => {
+          this.error = true
+          this.loading = false
+          this.notify('Load countries failed', 'error')
+        })
+    },
+    reload () {
+      if (this.error) {
+        this.error = false
+        this.load()
+      }
+    }
+  }
+}
+</script>
--- src/components/misc/index.js
+++ src/components/misc/index.js
@@ -1,8 +1,10 @@
+import VdCountries from './VdCountries'
 import VdElement from './VdElement'
 import VdLgas from './VdLgas'
 import VdStates from './VdStates'
 
 export default {
+  VdCountries,
   VdLgas,
   VdStates,
   VdElement
